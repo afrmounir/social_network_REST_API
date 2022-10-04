860b4427713bd6959eb76140dd7e641787f5c10e
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-04 18:38:15 +0200

    feat: send request & response & work with postman

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,15 @@
 exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: 'first', content: 'first post' }]
+  });
+};
 
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // create post in db
+  res.status(201).json({
+    message: 'Post created',
+    post: { id: new Date().toISOString(), title, content }
+  });
 };
