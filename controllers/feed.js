d3ac484fbd9505f59c3ab9b68955a49dbe68ec64
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-06 16:03:13 +0200

    feat: update posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
@@ -6,7 +8,7 @@ exports.getPosts = (req, res, next) => {
   Post
     .find()
     .then(posts => {
-      res.status(200).json({ posts })
+      res.status(200).json({ message: 'posts chargé avec succès', posts })
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -41,6 +43,7 @@ exports.createPost = (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(201).json({
+        message: 'post généré',
         post: result
       });
     })
@@ -70,4 +73,52 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('La validation a échoué, les données entrées sont incorrectes');
+    error.statusCode = 422;
+    throw error;
+  }
+  const { title, content } = req.body;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error('Pas de fichier');
+    error.statusCode = 422;
+    throw error;
+  }
+  Post
+    .findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Impossible de trouver un post');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then(result => res.status(200).json({ message: 'post mis à jour', post: result }))
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
