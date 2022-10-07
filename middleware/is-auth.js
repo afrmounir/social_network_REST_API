c73280865e00de46e86ddb4d50961db2600af56d
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-07 16:10:22 +0200

    feat: use and validate the JWT

---
+++ middleware/is-auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Non autorisé');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1]; // we add bearer for convention so we have to split it now
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, 'someLongSecretString'); //verify and decode
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Non autorisé');
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
