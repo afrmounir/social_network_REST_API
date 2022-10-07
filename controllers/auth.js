cb5657d4c5f70c250d6476a7e610e5315cbed43f
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-07 11:56:52 +0200

    feat: log in & generate JWTs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -19,6 +20,43 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then(result => res.status(201).json({ message: 'Utilisateur ajouté', userId: result._id }))
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User
+    .findOne({ email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('Utilisateur ou mot de passe incorrect');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isMatch => {
+      if (!isMatch) {
+        const error = new Error('Utilisateur ou mot de passe incorrect');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign({
+        email: loadedUser.email,
+        userId: loadedUser._id.toString()
+      },
+        'someLongSecretString',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token, userId: loadedUser._id.toString() })
+    })
     .catch(err => {
       if (!err.statusCode) {
         err.statusCode = 500;
