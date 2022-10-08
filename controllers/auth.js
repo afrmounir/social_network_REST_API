581841ceb4f6b668aa6f13b35ffbfd81b7afe93f
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-08 10:20:29 +0200

    feat: get validate and update status

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,3 +64,51 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => {
+  User
+    .findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('Utilisateur non trouvé');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.patchUserStatus = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('La validation a échoué, les données entrées sont incorrectes');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const newStatus = req.body.status;
+  User
+    .findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('Utilisateur non trouvé');
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then(() => res.status(200).json({ message: 'Status mis à jour' }))
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
