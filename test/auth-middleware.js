3f4aababd9831c105ffc7c8e21bd96f0be3f9172
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-15 17:27:04 +0200

    test: organize multiple tests for auth middleware

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,11 +2,22 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is available', function () {
-  const req = {
-    get: function () {
-      return null;
+describe('Auth Middleware', function () {
+  it('should throw an error if no authorization header is available', function () {
+    const req = {
+      get: function () {
+        return null;
+      }
     }
-  }
-  expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Non autorisé'); //we dont call it ourself, we pass the prepared reference so mocha and chai can call themselves
+    expect(authMiddleware.bind(this, req, {}, () => { })).to.throw('Non autorisé'); //we dont call it ourself, we pass the prepared reference so mocha and chai can call themselves
+  });
+
+  it('should throw an error if the authorization header is only one string', function () {
+    const req = {
+      get: function () {
+        return 'xyz';
+      }
+    }
+    expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
+  });
 });
