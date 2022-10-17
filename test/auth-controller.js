d8c942543c6bf70e826fd07c859d455d3b04c4f9
afrmounir <60487182+afrmounir@users.noreply.github.com>
2022-10-17 10:20:00 +0200

    test: use mocha hooks for cleaner tests

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -7,7 +7,23 @@ const authController = require('../controllers/auth');
 
 const MONGODB_URI = 'mongodb+srv://user815:9TMiDci0cy0Pd92m@cluster0.ns3cqzi.mongodb.net/test-socialnetwork?retryWrites=true&w=majority';
 
-describe('Auth Controller - Login', function () {
+describe('Auth Controller', function () {
+  before(function (done) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(() => {
+        const user = new User({
+          email: 'test@test.fr',
+          password: 'testtest',
+          name: 'test',
+          posts: [],
+          _id: '6349646e223b47fbf91674d7' // don't use an existing id in the db, id must be unique
+        });
+        return user.save();
+      })
+      .then(() => done());
+  })
+
   it('should throw an error (code 500) if accessing the database fails', function (done) { // add done as argument for async code
     sinon.stub(User, 'findOne');
     User.findOne.throws();
@@ -32,43 +48,32 @@ describe('Auth Controller - Login', function () {
   });
 
   it('should send a response with a valid user status for an existing user', function (done) {
-    mongoose
-      .connect(MONGODB_URI)
+    const req = { userId: '6349646e223b47fbf91674d7' };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) { // so we can call json on this object, like in the real controller
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status
+      }
+    };
+    authController
+      .getUserStatus(req, res, () => { })
       .then(() => {
-        const user = new User({
-          email: 'test@test.fr',
-          password: 'testtest',
-          name: 'test',
-          posts: [],
-          _id: '6349646e223b47fbf91674d7' // don't use an existing id in the db, id must be unique
-        });
-        return user.save();
-      })
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.userStatus).to.be.equal('Nouvel utilisateur');
+        done();
+      });
+  });
+
+  after(function (done) {
+    User.deleteMany({})//clean all the test db so we have a clean db for next test
       .then(() => {
-        const req = { userId: '6349646e223b47fbf91674d7' };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) { // so we can call json on this object, like in the real controller
-            this.statusCode = code;
-            return this;
-          },
-          json: function (data) {
-            this.userStatus = data.status
-          }
-        };
-        authController
-          .getUserStatus(req, res, () => { })
-          .then(() => {
-            expect(res.statusCode).to.be.equal(200);
-            expect(res.userStatus).to.be.equal('Nouvel utilisateur');
-            User.deleteMany({})//clean all the test db so we have a clean db for next test
-              .then(() => {
-                return mongoose.disconnect()
-              })
-              .then(() => done());
-          });
+        return mongoose.disconnect()
       })
-      .catch(err => console.log(err));
+      .then(() => done());
   });
 });
